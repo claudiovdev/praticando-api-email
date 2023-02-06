5ea012024f4f586ad11c170c248ead2c27acdba7
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2023-02-06 00:36:38 -0300

    Adicionando metodo de envio de email na controller e criando metodo na service

--- api-email/src/main/java/com/praticando/api/email/controllers/EmailController.java
+++ api-email/src/main/java/com/praticando/api/email/controllers/EmailController.java
@@ -1,8 +1,17 @@
 package com.praticando.api.email.controllers;
 
+import com.praticando.api.email.dtos.EmailDto;
+import com.praticando.api.email.models.EmailModel;
 import com.praticando.api.email.services.EmailService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class EmailController {
@@ -10,5 +19,11 @@ public class EmailController {
     @Autowired
     EmailService emailService;
 
-
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendEmail(@RequestBody @Valid EmailDto emailDto){
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendoEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
 }
