84a5627b897453a866703e8c699f9dad71837941
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2023-02-05 23:29:20 -0300

    Adicionando validações na DTO e removendo o emailId

--- api-email/src/main/java/com/praticando/api/email/dtos/EmailDto.java
+++ api-email/src/main/java/com/praticando/api/email/dtos/EmailDto.java
@@ -2,14 +2,27 @@ package com.praticando.api.email.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 
 @Data
 public class EmailDto {
-    private Long emailId;
+
+    @NotBlank
     private String ownerRef;
+
+    @NotBlank
+    @Email
     private String emailFrom;
+
+    @NotBlank
+    @Email
     private String emailTo;
+
+    @NotBlank
     private String subject;
+
+    @NotBlank
     private String text;
 }
